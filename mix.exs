81e17611d41afb6273ce07ea96abadcb797d26e4
James Harton <james@rabidtech.co.nz>
2017-01-05 14:07:05 +1300

    Bump version for hexdocs.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Vivid.Mixfile do
 
   def project do
     [app: :vivid,
-     version: "0.2.0",
+     version: "0.2.1",
      description: description,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
