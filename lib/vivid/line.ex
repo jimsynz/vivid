1488851261bc637bf0966070892588261f6e9a69
James Harton <james@rabidtech.co.nz>
2017-01-05 19:04:43 +1300

    Implement the point-in-polygon algorithm.

--- lib/vivid/line.ex
+++ lib/vivid/line.ex
@@ -1,7 +1,7 @@
 defmodule Vivid.Line do
   alias Vivid.{Line, Point}
   defstruct ~w(origin termination)a
-  import :math, only: [sqrt: 1, pow: 2]
+  import Vivid.Math
 
   @moduledoc ~S"""
   Represents a line segment between two Points in 2D space.
@@ -109,4 +109,59 @@ defmodule Vivid.Line do
     dy2 = line |> height |> pow(2)
     sqrt(dx2 + dy2)
   end
+
+  @doc """
+  Returns whether a point is on the line.
+
+  ## Example
+
+    iex> use Vivid
+    ...> Line.init(Point.init(1,1), Point.init(3,1))
+    ...> |> Line.on?(Point.init(2,1))
+    true
+
+    iex> use Vivid
+    ...> Line.init(Point.init(1,1), Point.init(3,1))
+    ...> |> Line.on?(Point.init(2,2))
+    false
+  """
+  @spec on?(Line.t, Point.t) :: boolean
+  def on?(%Line{origin: origin, termination: termination}=line, %Point{}=point) do
+    x_distance_point  = point.x  - termination.x
+    y_distance_point  = point.y  - termination.y
+    x_distance_origin = origin.x - termination.x
+    y_distance_origin = origin.y - termination.y
+    cross_product     = x_distance_point * y_distance_origin - x_distance_origin * y_distance_point
+
+    cross_product == 0.0
+  end
+
+  @doc """
+  Given a Line, find the Point on it that matches the X coordianate supplied.
+
+  Returns `nil` if there's no corresponding point.
+  """
+  @spec point_at_x(Line.t, number) :: Point.t | nil
+  def point_at_x(%Line{origin: %Point{x: x0, y: y0}, termination: %Point{x: x1, y: y1}}, x)
+  when (x0 <= x and x <= x1) or (x1 <= x and x <= x0) do
+    ratio = (x1 - x0) / (y1 - y0)
+    y = x * ratio
+    Point.init(x, y)
+  end
+  def point_at_x(_line, _point), do: nil
+
+  @doc """
+  Given a Line, find the Point on it that matches the Y coordianate supplied.
+
+  Returns `nil` if there's no corresponding point.
+  """
+  @spec point_at_y(Line.t, number) :: Point.t | nil
+  def point_at_y(%Line{origin: %Point{x: x0, y: y0}, termination: %Point{x: x1, y: y1}}, y)
+  when (y0 <= y and y <= y1) or (y1 <= y and y <= y0) do
+    ratio = (y1 - y0) / (x1 - x0)
+    x = y * ratio
+    Point.init(x, y)
+  end
+  def point_at_y(_line, _point), do: nil
+
 end
